b6fde39bf59d6d5a3ed8411d5dd9b26f1cd48e68
Aram <aramsaribekyan1@gmail.com>
2024-05-20 23:54:26 +0400

    Initial Commit

---
+++ MyWhatsUp/MyWhatsUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWhatsUpApp.swift
+//  MyWhatsUp
+//
+//  Created by Aram on 20.05.24.
+//
+
+import SwiftUI
+
+@main
+struct MyWhatsUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
